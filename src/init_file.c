a1f8fa150d83e3cae8936a5f418224f41fe23ee9
quentinhivanhoe <quentin.hivanhoe@epitech.eu>
2024-10-07 10:01:10 +0400

    [FIX]: fixing init file for unintialized varible

--- src/init_file.c
+++ src/init_file.c
@@ -3,9 +3,9 @@
 **          10/07/2024
 **
 **  Project : file_handler
-**  Description : transform the file into a string
-**  Date last update : 
-**  Subject last update :
+**  Description : allocate a file_t and fill it with filepath
+**  Date last update : 10/07/2024
+**  Subject last update : initialize nb_node ande the size of the file
 */
 #include "file_handler.h"
 #include <fcntl.h>
@@ -26,6 +26,8 @@ file_t *init_file(char *filepath)
     if (file == NULL)
         return NULL;
     file->file_name = strdup(filepath);
+    file->nb_node = 1;
+    file->size = 0;
     file->content = get_content(fd, &file->nb_node, &file->size);
     return file;
 }
