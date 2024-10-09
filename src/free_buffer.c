b47e256bf74dc2d33bf2ff92f504172e58e5e35b
quentinhivanhoe <quentin.hivanhoe@epitech.eu>
2024-10-09 08:23:47 +0400

    style(free_buffer): remove useless line from free buffer function

--- src/free_buffer.c
+++ src/free_buffer.c
@@ -8,24 +8,6 @@
 #include <stdio.h>
 #include "./file_handler.h"
 
-void __free_buffer(buffer_t *buffer)
-{
-    if (buffer == NULL)
-        return;
-    buffer = buffer->next;
-    if (buffer->index == buffer->next->index) {
-        free(buffer);
-        return;
-    }
-    while (buffer->index < buffer->next->index) {
-        buffer = buffer->next;
-        free(buffer->prev);
-    }
-    free(buffer->next);
-    free(buffer);
-    return;
-}
-
 void free_buffer(buffer_t *buffer)
 {
     unsigned int begin = 0;
